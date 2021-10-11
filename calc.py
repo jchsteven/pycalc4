37c05baa07a301574d883f5e21beb515d6be5573
Chenhui <chenhui.jin@monash.edu>
2021-10-11 12:39:43 +1100

    add a commit

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 # Perform simple arithmetic encoded in an input string:
 # '1 + 2' -> 3, or '1 - 2' -> -1.
-
+# add a comment
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
     num0, num1 = int(num0), int(num1)
@@ -11,7 +11,7 @@ def compute(expression):
     elif operator == '*':
         return num0 * num1
     elif operator == '/':
-        return num0 /  num1    
+        return num0 /  num1
     else:
         print('unknown operator!')
         return None
